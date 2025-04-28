c80231d5f9a00e7dc21e469c7f9c6ab74459e842
Samuel Newman <mozzius@protonmail.com>
2025-04-28 18:48:33 +0300

    write repro

--- App.js
+++ App.js
@@ -1,24 +1,11 @@
-import Animated, {
-  useSharedValue,
-  withTiming,
-  useAnimatedStyle,
-  Easing,
-} from "react-native-reanimated";
+import Animated, { ZoomIn, ZoomOut } from "react-native-reanimated";
 import { View, Button } from "react-native";
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
 
-export default function AnimatedStyleUpdateExample(props) {
-  const randomWidth = useSharedValue(10);
-
-  const config = {
-    duration: 500,
-    easing: Easing.bezier(0.5, 0.01, 0, 1),
-  };
-
-  const style = useAnimatedStyle(() => {
-    return {
-      width: withTiming(randomWidth.value, config),
-    };
-  });
+export default function Repro() {
+  const [show1, setShow1] = useState(false);
+  const [show2, setShow2] = useState(false);
 
   return (
     <View
@@ -26,21 +13,97 @@ export default function AnimatedStyleUpdateExample(props) {
         flex: 1,
         alignItems: "center",
         justifyContent: "center",
-        flexDirection: "column",
+        gap: 64,
+        backgroundColor: "black",
       }}
     >
-      <Animated.View
-        style={[
-          { width: 100, height: 80, backgroundColor: "black", margin: 30 },
-          style,
-        ]}
-      />
-      <Button
-        title="toggle"
-        onPress={() => {
-          randomWidth.value = Math.random() * 350;
+      <StatusBar style="light" />
+      <View
+        style={{
+          width: 200,
+          height: 200,
+          backgroundColor: "white",
+          position: "relative",
+          justifyContent: "center",
+          alignItems: "center",
         }}
-      />
+      >
+        <Button title="Show" onPress={() => setShow1(true)} />
+        {show1 && (
+          <View
+            style={{
+              backgroundColor: "cornflowerblue",
+              position: "absolute",
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              padding: 16,
+            }}
+          >
+            <Animated.View
+              entering={ZoomIn}
+              exiting={ZoomOut}
+              style={{
+                backgroundColor: "green",
+                borderRadius: 10,
+                width: "100%",
+                height: "100%",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Button
+                title="Hide"
+                onPress={() => setShow1(false)}
+                color="white"
+              />
+            </Animated.View>
+          </View>
+        )}
+      </View>
+      <View
+        style={{
+          width: 200,
+          height: 200,
+          backgroundColor: "white",
+          position: "relative",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Button title="Show (no animation)" onPress={() => setShow2(true)} />
+        {show2 && (
+          <View
+            style={{
+              backgroundColor: "cornflowerblue",
+              position: "absolute",
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              padding: 16,
+            }}
+          >
+            <View
+              style={{
+                backgroundColor: "green",
+                borderRadius: 10,
+                width: "100%",
+                height: "100%",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Button
+                title="Hide"
+                onPress={() => setShow2(false)}
+                color="white"
+              />
+            </View>
+          </View>
+        )}
+      </View>
     </View>
   );
 }
